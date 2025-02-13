cc2c5ccd5836eab52e00f1053f6003b835b186f5
Lucas Moraes <lucasmoraesostemberg761@gmail.com>
2025-02-13 15:31:33 -0400

    tipagem

---
+++ Tipando Objetos/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(name, age) {
+    console.log(`Hello, ${name.toUpperCase()}!`);
+}
+greet('Lucas');
---
+++ Tipando Objetos/index.ts
@@ -0,0 +1,12 @@
+type Person = {
+    name: string
+    age: number
+    contacts: string[]
+    isAlive?: boolean
+}
+
+const person: Person= {
+    age: 22,
+    name: "Lucas",
+    contacts: [],
+}
